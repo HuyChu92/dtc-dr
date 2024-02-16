8f21444219eeb3b7922698e0795968c8636d0f0a
Huy Chu <1135546chu@zuyd.nl>
2024-02-16 11:12:12 +0100

    changes

--- digitaltwins/api/urls.py
+++ digitaltwins/api/urls.py
@@ -5,12 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('dtc-configurator/', views.dtc_configurator, name='dtc-configurator'),
-    path('saveMeasurement/', views.saveMeasurement, name='saveMeasurement'),
-    path('fetchLatestMeasurement/', views.fetchLatestMeasurement, name='fetchLatestMeasurement'),
-    path('fetchAllMeasurements/', views.fetchAllMeasurements, name='fetchAllMeasurements'),
-    path('fetchInputTestData/', views.fetchInputTestData, name='fetchInputTestData'),
-    path('fetchAverageMachine1/', views.fetchAverageMachine1, name='fetchAverageMachine1'),
     path('getPrediction/', views.getPrediction, name='getPrediction'),
     path('trainModel/', views.trainModel, name='trainModel'),
     path('fetchDatasets/', views.fetchDatasets, name='fetchDatasets'),
